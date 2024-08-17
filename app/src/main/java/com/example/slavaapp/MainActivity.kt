792275bb712722ffea03fa8362fcd002116fc393
Kinder522 <slava.vinogradov.2009@mail.ru>
2024-08-17 15:42:14 +0300

    All rework

--- app/src/main/java/com/example/slavaapp/MainActivity.kt
+++ app/src/main/java/com/example/slavaapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.slavaapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,26 +23,94 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val label = findViewById<TextView>(R.id.mainlabel)
-        val buttonNumber_1: EditText = findViewById(R.id.num1)
-        val buttonNumber_2 :EditText= findViewById(R.id.num2)
-        val buttonDeistvie :EditText= findViewById(R.id.deisvie)
-        val buttonRavno:Button = findViewById(R.id.button)
+        val label = findViewById<TextView>(R.id.textView2)
+        val num1:Button = findViewById(R.id.button1)
+        val num2:Button = findViewById(R.id.button2)
+        val num3:Button = findViewById(R.id.button3)
+        val num4:Button = findViewById(R.id.button6)
+        val num5:Button = findViewById(R.id.button4)
+        val num6:Button = findViewById(R.id.button5)
+        val num7:Button = findViewById(R.id.button7)
+        val num8:Button = findViewById(R.id.button9)
+        val num9:Button = findViewById(R.id.button8)
+        val num0:Button = findViewById(R.id.button0)
+        val numPlus:Button = findViewById(R.id.buttonPlus)
+        val numMinus:Button = findViewById(R.id.buttonMinus)
+        val numMultiplication:Button = findViewById(R.id.buttonMultiplication)
+        val numRovno:Button = findViewById(R.id.buttonEvenly)
+        val numC:Button = findViewById(R.id.buttonC)
+        var number1 = label.text.toString().trim()
+        var deystvie = ""
 
+        num1.setOnClickListener{
+            val text  = "1"
+            label.text = (label.text.toString()) + text
+        }
+        num2.setOnClickListener{
+            val text  = "2"
 
-        buttonRavno.setOnClickListener{
-            val num1 = buttonNumber_1.text.toString().trim()
-            val num2 = buttonNumber_2.text.toString().trim()
-            val deistvie = buttonDeistvie.text.toString().trim()
-            if (deistvie == "+")
-                label.text = (num1.toInt() + num2.toInt()).toString()
-            else if (deistvie == "-")
-                label.text = (num1.toInt() - num2.toInt()).toString()
-            else if (deistvie == "*")
-                label.text = (num1.toInt() * num2.toInt()).toString()
-            else if (deistvie == "/")
-                label.text = (num1.toInt() / num2.toInt()).toString()
+            label.text = (label.text.toString()) + text
+        }
+        num3.setOnClickListener{
+            val text  = "3"
+            label.text = (label.text.toString()) + text
+        }
+        num4.setOnClickListener{
+            val text  = "4"
+            label.text = (label.text.toString()) + text
+        }
+        num5.setOnClickListener{
+            val text  = "5"
+            label.text = (label.text.toString()) + text
+        }
+        num6.setOnClickListener{
+            val text  = "6"
+            label.text = (label.text.toString()) + text
+        }
+        num7.setOnClickListener{
+            val text  = "7"
+            label.text = (label.text.toString()) + text
+        }
+        num8.setOnClickListener{
+            val text  = "8"
+            label.text = (label.text.toString()) + text
+        }
+        num9.setOnClickListener{
+            val text  = "9"
+            label.text = (label.text.toString()) + text
+        }
+        num0.setOnClickListener{
+            val text  = "0"
+            label.text = (label.text.toString()) + text
+        }
+        numPlus.setOnClickListener(){
+            number1 = label.text.toString().trim()
+            label.text = ""
+            deystvie = "+"
+        }
+        numMinus.setOnClickListener(){
+            number1 = label.text.toString().trim()
+            label.text = ""
+            deystvie = "-"
+        }
+        numMultiplication.setOnClickListener(){
+            number1 = label.text.toString().trim()
+            label.text = ""
+            deystvie = "*"
+        }
+        numC.setOnClickListener(){
+            number1 = ""
+            label.text = ""
+        }
 
+        numRovno.setOnClickListener{
+            val number2 = label.text.toString().trim()
+            if(deystvie == "+"){
+                label.text = (number1.toInt() + number2.toInt()).toString()
+            }else if (deystvie == "-")
+                label.text = (number1.toInt() - number2.toInt()).toString()
+            else if (deystvie == "*")
+                label.text = (number1.toInt() * number2.toInt()).toString()
         }
 
 
